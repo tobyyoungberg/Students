6ff9f87c1e4058e7c359dd70af62b30c5f3bfe2f
Andrew Madsen <andrew@openreelsoftware.com>
2022-04-18 07:48:36 -0600

    Initial Commit

---
+++ Students/StudentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentsApp.swift
+//  Students
+//
+//  Created by Andrew R Madsen on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct StudentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
